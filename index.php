96c6a702c18fc5a161fa178c90651ff2ff698a21
Jérémie ANDRIA <79089189+Natjay0230@users.noreply.github.com>
2021-10-25 10:02:31 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <body>
 <nav class="navbar navbar-expand-lg navbar-light py-3 bg-light"  id="mainNav">
             <div class="container px-4 px-lg-5">
+                <link rel="shortcut icon" type="image/x-icon" href="http://daraj-afaas.herokuapp.com/img/3.png" />
                 <a class="navbar-brand" href="index.php">DARAJ</a>
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse " id="navbarResponsive">
